eb8ead930e8fcd4f8eb56bef372a628f896ae6b1
hakanolgun <hakanolgun@tutamail.com>
2023-03-17 17:29:53 +0300

    add Episodes and Episode screens

--- src/screens/EpisodeScreen.tsx
+++ src/screens/EpisodeScreen.tsx
@@ -1,10 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import React from 'react';
+import ShowMsg from '../components/common/ShowMsg';
+import base from '../styles/base';
+import useEpisode from '../hooks/useEpisode';
 
-const EpisodeScreen = () => {
+const EpisodeScreen = ({route, navigation}) => {
+  const [res, loading, error] = useEpisode(route.params.id);
+
+  const renderCharacters = () => <Text>Character</Text>;
+
+  if (loading || !res) {
+    return <ShowMsg full loading />;
+  }
+  if (error) {
+    return <ShowMsg full err msg={error} />;
+  }
   return (
-    <View>
-      <Text>EpisodeScreen</Text>
+    <View style={base.container}>
+      <View>
+        <Text>{res.name}</Text>
+        <Text>{res.air_date}</Text>
+        <Text>{res.episode}</Text>
+      </View>
+      <FlatList
+        data={res.characters}
+        keyExtractor={item => item}
+        renderItem={renderCharacters}
+      />
     </View>
   );
 };
--- src/screens/EpisodesScreen.tsx
+++ src/screens/EpisodesScreen.tsx
@@ -1,25 +1,53 @@
-import {View, Text, FlatList} from 'react-native';
-import React from 'react';
+import {View, FlatList} from 'react-native';
+import React, {useState} from 'react';
 import base from '../styles/base';
 import useEpisodes from '../hooks/useEpisodes';
 import ShowMsg from '../components/common/ShowMsg';
+import {IEpisode} from '../interface/episode';
+import EpisodeCard from '../components/episode/EpisodeCard';
+import Pagination from '../components/common/Pagination';
+import {getEpisodes} from '../api/api';
 
 const EpisodesScreen = () => {
-  const [res, loading, error] = useEpisodes();
-  React.useEffect(() => {
-    console.log('res', res);
-  }, [res]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [res, loading, error, fetchAgain] = useEpisodes('1');
 
-  const renderCards = ({item}) => <Text style={base.text}>{item.episode}</Text>;
+  const renderCards = ({item}: {item: IEpisode}) => (
+    <EpisodeCard episode={item} />
+  );
 
+  const handlePrev = async (prev: string) => {
+    if (prev) {
+      await fetchAgain(getEpisodes(prev.charAt(prev.length - 1)));
+      setCurrentPage(p => p - 1);
+    }
+  };
+  const handleNext = async (next: string) => {
+    if (next) {
+      await fetchAgain(getEpisodes(next.charAt(next.length - 1)));
+      setCurrentPage(p => p + 1);
+    }
+  };
+
+  if (loading || !res) {
+    return <ShowMsg full loading />;
+  }
+  if (error) {
+    return <ShowMsg err msg={error} />;
+  }
   return (
     <View style={base.container}>
-      {loading ? <ShowMsg full loading /> : null}
-      {error ? <ShowMsg err msg={error} /> : null}
       <FlatList
-        data={res?.results ?? []}
-        keyExtractor={item => item.id}
+        data={res.results ?? []}
+        keyExtractor={item => String(item.id)}
         renderItem={renderCards}
+        showsVerticalScrollIndicator={false}
+      />
+      <Pagination
+        currentPage={currentPage}
+        info={res.info}
+        onPrev={handlePrev}
+        onNext={handleNext}
       />
     </View>
   );
