1321a99e5da8cb8f7384864bea270fcef7e42ae3
hakanolgun <hakanolgun@tutamail.com>
2023-03-18 16:34:37 +0300

    Remove unused functions from utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,3 @@
-export const createAvatarUrl = (url: string) => {
-  const array = url.split('/');
-  const charID = array[array.length - 1];
-  return `https://rickandmortyapi.com/api/character/avatar/${charID}.jpeg`;
-};
-
 export const getCharID = (url: string) => {
   const array = url.split('/');
   const charID = array[array.length - 1];
