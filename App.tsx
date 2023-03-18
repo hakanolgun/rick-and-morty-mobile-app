8c0cd99e986c8f8d107c1b8b9e1492bb6b67abcc
hakanolgun <hakanolgun@tutamail.com>
2023-03-18 11:28:26 +0300

    configure redux store

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './src/navigation/TabNavigation';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistStore} from 'redux-persist';
+import {store} from './src/store/store';
+
+let persistor = persistStore(store);
 
 const App = () => {
+  StatusBar.setBarStyle('light-content', true);
   return (
-    <NavigationContainer>
-      <TabNavigation />
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <TabNavigation />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 };
 
